952e3d6c13d26ae24d6ae9e09d558d987c2365d5
Claudia Velasquez <calevd@gmail.com>
2019-05-22 17:19:04 -0500

    Actualizacion de diseño coutas sociales

--- View/card-cuotas-sociales.php
+++ View/card-cuotas-sociales.php
@@ -1,90 +1,97 @@
 <!-- inicio de card 1 -->
-<form id="">
-
 <input type="hidden" name="Usuario" value="JORGE.WINDER">
  
-    <div class="card white">
-            <div class="card-content">
-                <!--<span class="card-title center-align" style="border-left: 7px solid #e85b21;border-right: 7px solid #e85b21;">INGRESAR</span>-->
-                <span class="card-title" style="color: #f39c12;padding-bottom: 10px;"></span>
-                
-                    <div class="row">
-                        <div class="input-field col s12">
-                            <select>
-                                <option value="" disabled selected>Choose your option</option>
-                                <option value="1">Option 1</option>
-                                <option value="2">Option 2</option>
-                                <option value="3">Option 3</option>
-                            </select>
-                            <label>Operaciones</label>
-                        </div>
-                        
-                    </div>
-
-
-                    <div class="row">
-                        <form class="col s12">
-                            <div class="row">
-                                <div class="input-field col s6">
-                                    <input placeholder="Placeholder" id="first_name" type="text" class="validate">
-                                    <label for="first_name">Còdigo</label>
-                                </div>
-                            </div>
-                        </form>
-                    </div>
-
-                    <div class="row">
-                        <form class="col s12">
-                            <div class="row">
-                                <div class="input-field col s6">
-                                    <input placeholder="Placeholder" id="first_name" type="text" class="validate">
-                                    <label for="first_name">Apellidos</label>
-                                </div>
-                            </div>
-                        </form>
-                    </div>
-
-                    <div class="row">
-                        <form class="col s12">
-                            <div class="row">
-                                <div class="input-field col s6">
-                                    <input placeholder="Placeholder" id="first_name" type="text" class="validate">
-                                    <label for="first_name">Nombres</label>
-                                </div>
-                            </div>
-                        </form>
-                    </div>
-
-                    <div class="row">
-                        <form class="col s6">
-                            <div class="row">
-                                <div class="input-field col s6">
-                                    <input placeholder="Placeholder" id="first_name" type="text" class="validate">
-                                    <label for="first_name">Origen</label>
-                                </div>
-                            </div>
-                        </form>
-                    </div>
-
-                    <div class="row">
-                        <form class="col s6">
-                            <div class="row">
-                                <div class="input-field col s6">
-                                    <input placeholder="Placeholder" id="first_name" type="text" class="validate">
-                                    <label for="first_name">Categoría</label>
-                                </div>
-                            </div>
-                        </form>
-                    </div>
-
-
-
-
-
-
-
-
-            </div>
+    <div class="row">
+        <div class="input-field col s12">
+            <select>
+                <option value="" disabled selected>Choose your option</option>
+                <option value="1">Option 1</option>
+                <option value="2">Option 2</option>
+                <option value="3">Option 3</option>
+            </select>
+            <label>Operaciones</label>
+        </div>
     </div>
 
-</form>
+
+    <div class="row">
+        <form class="col s6">
+            <div class="row">
+                <div class="input-field col s8">
+                    <input placeholder="Placeholder" id="first_name" type="text" class="validate">
+                    <label for="first_name">Código</label>
+                </div>
+                <a class="btn-floating btn-medium waves-effect waves-light blue"><i class="material-icons">search</i></a>
+            </div>
+        </form>
+
+        <form action="#">
+            <p>
+                <label>
+                    <input type="checkbox" />
+                    <span>PAGO ANUAL</span>
+                </label>
+            </p>
+        </form>
+    </div>
+
+    <div class="row">
+        <form class="col s4">
+            <div class="row">
+                <div class="input-field col s12">
+                    <input placeholder="Placeholder" id="first_name" type="text" class="validate">
+                    <label for="first_name">Apellidos</label>
+                </div>
+            </div>
+        </form>
+    </div>
+
+    <div class="row">
+        <form class="col s4">
+            <div class="row">
+                <div class="input-field col s12">
+                    <input placeholder="Placeholder" id="first_name" type="text" class="validate">
+                    <label for="first_name">Nombres</label>
+                </div>
+            </div>
+        </form>
+    </div>
+
+    
+    
+    <div class="row">
+        <form class="col s6">
+            <div class="row">
+                <div class="input-field col s6">
+                    <input placeholder="Placeholder" id="first_name" type="text" class="validate">
+                    <label for="first_name">Origen</label>
+                </div>
+                <div class="input-field col s6">
+                    <input placeholder="Placeholder" id="first_name" type="text" class="validate">
+                    <label for="first_name">Categoría</label>
+                </div>
+            </div>
+        </form>
+    </div>
+
+    <div class="row">
+        <form class="col s6">
+            <div class="row">
+                <div class="input-field col s6">
+                    <input placeholder="Placeholder" id="first_name" type="text" class="validate">
+                    <label for="first_name">Estado</label>
+                </div>
+                
+                <div class="col s3">
+                    <a class="waves-effect waves-light btn">Imprimir Boleta</a>
+                </div>
+
+                <div class="col s3">    
+                    <a class="waves-effect waves-light btn disabled">Cancelar Boleta</a>
+                </div>
+                
+            </div>
+        </form>
+    </div>
+
+                    
